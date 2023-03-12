2127d781e959d685a64addf16b6c64c0ab00aee8
D.Vin <94095647+Dalonson@users.noreply.github.com>
2023-03-12 01:14:06 -0800

    html boilerplating

--- src/data.js
+++ src/data.js
@@ -1,72 +1,26 @@
-export const chatData = {
-  currentUser: {
-    image: {
-      png: "./images/avatars/image-juliusomo.png",
-      webp: "./images/avatars/image-juliusomo.webp",
-    },
-    username: "juliusomo",
+export const chatData = [
+  {
+    handle: `@limoTintedBlue 💎`,
+    profilePic: `images/troll.jpg`,
+    chatText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
+          Guaranteed return on investment. HMU DMs open!!`,
+    replies: [],
+    isLiked: false,
+    uuid: "4b161eee-c0f5-4545-9c4b-8562944223ee",
   },
-  comments: [
-    {
-      id: 1,
-      content:
-        "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-      createdAt: "1 month ago",
-      score: 12,
-      user: {
-        image: {
-          png: "./images/avatars/image-amyrobson.png",
-          webp: "./images/avatars/image-amyrobson.webp",
-        },
-        username: "amyrobson",
-      },
-      replies: [],
-    },
-    {
-      id: 2,
-      content:
-        "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-      createdAt: "2 weeks ago",
-      score: 5,
-      user: {
-        image: {
-          png: "./images/avatars/image-maxblagun.png",
-          webp: "./images/avatars/image-maxblagun.webp",
-        },
-        username: "maxblagun",
-      },
-      replies: [
-        {
-          id: 3,
-          content:
-            "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-          createdAt: "1 week ago",
-          score: 4,
-          replyingTo: "maxblagun",
-          user: {
-            image: {
-              png: "./images/avatars/image-ramsesmiron.png",
-              webp: "./images/avatars/image-ramsesmiron.webp",
-            },
-            username: "ramsesmiron",
-          },
-        },
-        {
-          id: 4,
-          content:
-            "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-          createdAt: "2 days ago",
-          score: 2,
-          replyingTo: "ramsesmiron",
-          user: {
-            image: {
-              png: "./images/avatars/image-juliusomo.png",
-              webp: "./images/avatars/image-juliusomo.webp",
-            },
-            username: "juliusomo",
-          },
-        },
-      ],
-    },
-  ],
-};
+  {
+    handle: `@NoBody$pecial ✅`,
+    profilePic: `images/musk.png`,
+    chatText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
+    isLiked: false,
+    isRetweeted: false,
+    uuid: "3c23454ee-c0f5-9g9g-9c4b-77835tgs2",
+  },
+  {
+    handle: `@NoobCoder12`,
+    profilePic: `images/flower.png`,
+    chatText: `Are you a coder if you only know HTML?`,
+    isLiked: false,
+    uuid: "8hy671sff-c0f5-4545-9c4b-1237gyys45",
+  },
+];
