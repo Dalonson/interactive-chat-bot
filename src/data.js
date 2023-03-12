245614428fb5e14fe18cec6839e373c18111571d
D.Vin <94095647+Dalonson@users.noreply.github.com>
2023-03-12 03:05:27 -0700

    dependencies

--- src/data.js
+++ src/data.js
@@ -6,6 +6,7 @@ export const chatData = [
           Guaranteed return on investment. HMU DMs open!!`,
     replies: [],
     isLiked: false,
+    time: "",
     uuid: "4b161eee-c0f5-4545-9c4b-8562944223ee",
   },
   {
@@ -13,7 +14,7 @@ export const chatData = [
     profilePic: `images/musk.png`,
     chatText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
     isLiked: false,
-    isRetweeted: false,
+    time: "",
     uuid: "3c23454ee-c0f5-9g9g-9c4b-77835tgs2",
   },
   {
@@ -21,6 +22,7 @@ export const chatData = [
     profilePic: `images/flower.png`,
     chatText: `Are you a coder if you only know HTML?`,
     isLiked: false,
+    time: "",
     uuid: "8hy671sff-c0f5-4545-9c4b-1237gyys45",
   },
 ];
--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
 "use strict";
+import { chatData } from "./data";
+
+// timestamp variables
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth() + 1;
+const day = now.getDate();
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const amOrPm = hours >= 12 ? "PM" : "AM";
+const twelveHourFormat = hours % 12 || 12;
+const formattedTime = `${twelveHourFormat}:${minutes} ${amOrPm}`;
+const formattedDate = `${month}-${day}-${year}`;
+
+const timestamp = `${formattedDate} ${formattedTime}`;
+
+//
+
+const chatInput = document.getElementById("chat-input");
+const sendButton = documement.getElementById("send-button");
+
+sendButton.addEventListener("click", function () {
+  console.log(chatInput.value);
+});
